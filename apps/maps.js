a7b52b677471fc1611ffb971c91fe6390ed9e402
ShibalINU <164317399+ShibalINU@users.noreply.github.com>
2024-11-26 14:22:57 +0800

    Update maps.js

--- apps/maps.js
+++ apps/maps.js
@@ -54,6 +54,7 @@ document.getElementById("maps.js").addEventListener("click", () => {
         src:'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d62041.73973414218!2d123.1801303!3d13.6206923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sph!4v1732601655767!5m2!1sen!2sph',
         width:"100%",
         height:"100%",
+        style: "border:none;"
     },
         "iframe",
         section)
